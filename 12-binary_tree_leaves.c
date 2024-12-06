71b24c3d942c81b94d45168b38901d7cbcc85dbe
salmachikima <salmamarokti888@gmail.com>
2024-12-06 05:17:10 +0100

    so much leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf -  a function that checks if a node is a leaf.
- *@node: is a pointer to the node to check.
- * Return: return 1 if node is a leaf, otherwise 0.
+ * binary_tree_is_leaf -  fnctn that checks if a node is a leaf
+ *@node: is a pointer to the node to check
+ * Return: 1 if node is leaf else 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
@@ -18,8 +18,8 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 /**
  * binary_tree_leaves - number of leaves
- *@tree: pointer to the root node of the tree to count the number of leaves.
- * Return: If tree is NULL, the function must return 0.
+ *@tree: pointer to the root node for counting the leafs
+ * Return: If tree NULL return 0
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
