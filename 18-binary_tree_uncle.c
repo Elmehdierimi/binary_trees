5fa425bf9017ca9a6e9efeeb2cde1c3ed5df50ed
salmachikima <salmamarokti888@gmail.com>
2024-12-06 05:39:40 +0100

    she's looking for her uncle too? hahahah

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - finds the sibling of a node.
- *@node: pointer to the node to find the sibling.
- * Return: pointer to the sibling node.
+ * binary_tree_sibling - finds the sibling of a node
+ *@node: pointer to the node to find sibling
+ * Return: sibling node pointer
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
@@ -16,9 +16,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 }
 /**
- * binary_tree_uncle - function that finds the uncle of a node.
- *@node: pointer to the node to find the uncle.
- * Return: pointer to the uncle node.
+ * binary_tree_uncle - function that finds the uncle of the node
+ *@node: pointer to the node to find the uncle
+ * Return: the uncle node pointer
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
