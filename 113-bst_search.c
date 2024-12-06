8d2b0d0f4b05e3856cec1ffa1a08d4c2fc3f547e
salmachikima <salmamarokti888@gmail.com>
2024-12-06 05:58:52 +0100

    Update 113-bst_search.c

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * bst_search -function find node in a tree
- * @tree: root of the tre to evaluate
+ * bst_search -fnctn to find node in a tree
+ * @tree: root of the evaluated tree
  * @value: node to find
- * Return: 1 if exits 0 if no
+ * Return: 1 if yes 0 if no
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
