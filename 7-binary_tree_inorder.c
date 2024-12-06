476e154cd2c551edf00d399e743da5f6bc2e8cbe
salmachikima <salmamarokti888@gmail.com>
2024-12-06 05:03:22 +0100

    more...

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through binary tree using in-order
- *@tree: pointer to the root node of the tree to traverse.
- *@func: pointer to a function to call for each node.
+ * binary_tree_inorder - fnctn using in-order goes through that binary tree
+ *@tree: pointer to the root node as we said earlier
+ *@func: point to a fnctn to call for each node
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
