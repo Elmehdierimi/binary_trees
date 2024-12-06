3e437d0355a6f82f3d19910a005f0c9aa166e196
salmachikima <salmamarokti888@gmail.com>
2024-12-06 05:15:05 +0100

    ssssss

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree.
- *@tree: pointer to the root node of the tree to measure the size.
- * Return: if tree is NULL, must return 0.
+ * binary_tree_size - fnctn that measures the size of a binary tree
+ *@tree: pointer to the root node to mesure
+ * Return: if tree NULL RETURN 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
