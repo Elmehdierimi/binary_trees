503437434c65605f91083a151e043ff5694cf63e
salmachikima <salmamarokti888@gmail.com>
2024-12-06 05:47:12 +0100

    puuuurfect~

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_height - measures the height of a binary tree
- *@tree: pointer to the root node for height mesure.
+ *@tree: pointer to the root node for height mesure
  * Return: if tree is NULL return 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -23,9 +23,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 }
 /**
- * binary_tree_size - measures the size of a binary tree.
- *@tree: pointer to the root node of the tree to measure the size.
- * Return: if tree is NULL, must return 0.
+ * binary_tree_size - measures the size of a binary tree
+ *@tree: pointer to the root node of the tree to measure the size
+ * Return: if tree is NULL return 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
